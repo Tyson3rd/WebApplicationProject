a5d67dbd62e1b8c46fbf126d52a5bd28d2c6102f
Tyson Code Cademy <Tyson3rd@Michelles-MacBook-Air.local>
2021-06-17 11:21:28 -0500

    Final commit

---
+++ src/main/java/com/tyson/WAP/WapApplication.java
@@ -0,0 +1,13 @@
+package com.tyson.WAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyson/WAP/WapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyson.WAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
